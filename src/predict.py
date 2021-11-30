cc0c31ee4205069db171cd942281b05a33c98a2d
sara-shiho <saraho2021@u.northwestern.edu>
2021-11-29 22:08:35 -0600

    significant progress in prediction code

--- src/predict.py
+++ src/predict.py
@@ -5,38 +5,42 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.utils import resample
 
 from process import clean_text
-from embeddings import get_vectors_glove, counter_embedding, get_vectors_from_count, get_cosine_sim
+from embeddings import get_vectors_glove, counter_embedding, get_vectors_from_count, get_cosine_sim, google
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+RANDOM_STATE = 414
 
-def confusion_mat(labeled):
-    cm1 = confusion_matrix(labeled['label'], labeled['predict'])
+
+def upsample(train_df):
+    positive = train_df.loc[train_df['label'] == 1]
+    negative = train_df.loc[train_df['label'] == 0]
+
+    positive_upsampled = resample(positive, random_state=RANDOM_STATE,
+                                  n_samples=len(negative) - len(positive))
+
+    return pd.concat([positive, positive_upsampled, negative])
+
+
+def confusion_mat(actual, predicted):
+    cm1 = confusion_matrix(actual, predicted)
     total1 = sum(sum(cm1))
 
     accuracy = (cm1[0, 0]+cm1[1, 1])/total1
     sensitivity = cm1[0, 0]/(cm1[0, 0] + cm1[0, 1])
     specificity = cm1[1, 1]/(cm1[1, 0] + cm1[1, 1])
-    f1 = f1_score(labeled['label'], labeled['predict'])
+    f1 = f1_score(actual, predicted)
 
     return {'accuracy': round(accuracy, 3),
             'sensitivity': round(sensitivity, 3),
             'specificity': round(specificity, 3),
             'f1': round(f1, 3)}
-
-
-def cutoff_predict(data, cutoff):
-
-    data['predict'] = data['sim'] > cutoff
-    print("there are %i total matches", len(data))
-    print("there are %i actual matches", data['label'].sum())
-    print("predicted %i relevant matches", data['predict'].sum())
-
-    return data
 
 
 def tfidf_transform(vectorizer, text):
@@ -81,12 +85,7 @@ def tfidf_run():
     labeled['wiki'] = labeled['wiki'].apply(clean_text).apply(remove_low_tfidf)
 
 
-if __name__ == '__main__':
-
-    labeled = pd.read_csv('../data/labeled.csv')
-    labeled = labeled.fillna('')
-
-    # Experiment 1: similarity cutoff
+def exp_similarity_cutoff(labeled):
     sim = []
     for i, row in labeled[['news', 'wiki']].iterrows():
         clean_news = clean_text(row['news'])
@@ -100,25 +99,86 @@ if __name__ == '__main__':
         sim.append(count_similarities)
 
     labeled['sim'] = sim
+    return labeled
 
-    predicted = cutoff_predict(labeled, 0.75)
-    print(confusion_mat(predicted))
 
-    # Define X and y
-    # X is the distance between news and wiki text as vectors
-    X = labeled['news'].apply(get_vectors_glove) - labeled['wiki'].apply(get_vectors_glove)
-    X = pd.DataFrame(X.to_list())
+def exp_ml_prediction(X_train, X_test, y_train, y_test):
 
-    y = labeled['label']
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-    # Experiment 2: Logistic Regression
-    lr = LogisticRegression(random_state=0).fit(X, y)
+    logger.info('Logistic Regression')
+    lr = LogisticRegression(random_state=RANDOM_STATE, max_iter=1000).fit(X_train_scaled, y_train)
+    predicted = lr.predict(X_train_scaled)
+    logger.info('train predictions:')
+    annotate(y_train, predicted)
 
-    labeled['predict'] = lr.predict(X)
-    print(confusion_mat(labeled))
+    predicted = lr.predict(X_test_scaled)
+    logger.info('test predictions:')
+    annotate(y_test, predicted)
 
-    # Experiment 3: SVC
-    lr = SVC(random_state=0).fit(X, y)
+    logger.info('Support Vector Classifier')
+    svc = SVC(random_state=RANDOM_STATE).fit(X_train_scaled, y_train)
+    predicted = svc.predict(X_train_scaled)
+    logger.info('train predictions:')
+    annotate(y_train, predicted)
 
-    labeled['predict'] = lr.predict(X)
-    print(confusion_mat(labeled))
+    predicted = svc.predict(X_test_scaled)
+    logger.info('test predictions:')
+    annotate(y_test, predicted)
+
+    return lr, svc
+
+
+def annotate(actual, predicted):
+    print(confusion_mat(actual, predicted))
+
+    print("there are %i total matches", len(actual))
+    print("there are %i actual matches", actual.sum())
+    print("predicted %i relevant matches", predicted.sum())
+
+
+def run(train, test):
+    train = train.fillna('')
+    test = test.fillna('')
+    train = upsample(train)
+
+    y_train = train['label'].copy()
+    y_test = test['label'].copy()
+
+    # Experiment 1: similarity cutoff
+    logger.info('Experiment 1')
+    labeled_similarity = exp_similarity_cutoff(train)
+    predicted = (labeled_similarity['sim'] > 0.3).astype(int)
+    annotate(y_train, predicted)
+
+    # Experiment 2: Glove Vectors
+    logger.info('Experiment 2')
+
+    X_train = train['news'].apply(get_vectors_glove) - train['wiki'].apply(get_vectors_glove)
+    X_train = pd.DataFrame(X_train.to_list())
+
+    X_test = test['news'].apply(get_vectors_glove) - test['wiki'].apply(get_vectors_glove)
+    X_test = pd.DataFrame(X_test.to_list())
+
+    lr, svc = exp_ml_prediction(X_train, X_test, y_train, y_test)
+
+    # Experiment 3: Google Vectors
+    logger.info('Experiment 3')
+    X_train = google(train['news']) - google(train['wiki'])
+    X_test = google(test['news']) - google(test['wiki'])
+
+    lr, svc = exp_ml_prediction(X_train, X_test, y_train, y_test)
+
+
+
+if __name__ == '__main__':
+
+    labeled = pd.read_csv('../data/labeled.csv')
+
+    test1 = pd.read_csv('../data/wikinews_11-24-2021.csv')
+    test2 = pd.read_csv('../data/wikinews_11-29-2021.csv')
+    test = test1.append(test2)
+
+    run(labeled, test)
