e2ad6ad346b74b03a34dd716be6633d835e460f1
sara-shiho <saraho2021@u.northwestern.edu>
2021-11-29 23:34:13 -0600

    update app to work with new data; major changes still needed

--- app/api.py
+++ app/api.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask import render_template, jsonify
 import json
+import tensorflow_hub as hub
 
 app = Flask(__name__)
 
 
+if 'google' not in globals():
+    google = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+
 @app.route("/")
 def index():
-    with open('results.json') as f:
+    with open('../artifacts/results.json') as f:
         results = json.load(f)
     return render_template('index.html', results=results)
 
